28298614ef0f7f5918feec8fdbd7d221d94c57aa
Abdelrahman <Aa5282243@gmail.com>
2025-11-01 20:11:13 +0200

    Added Sentry

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,26 @@ import 'package:movie_app/core/di/injection_container.dart';
 import 'package:movie_app/features/home/data/models/hive_model/movie_hive_model.dart';
 import 'package:movie_app/movie_app.dart';
 import 'package:movie_app/shared/cubit/setting_cubit.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(MovieHiveModelAdapter());
   await initDependencyInjection();
-  runApp(
-    BlocProvider<ThemeCubit>(
-      create: (BuildContext context) => ThemeCubit(),
-      child: const MovieApp(),
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          '';
+    },
+    appRunner: () => runApp(
+      SentryWidget(
+        child: BlocProvider<ThemeCubit>(
+          create: (BuildContext context) => ThemeCubit(),
+          child: const MovieApp(),
+        ),
+      ),
     ),
   );
 }
